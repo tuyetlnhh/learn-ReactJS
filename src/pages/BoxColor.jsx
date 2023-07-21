e3e4b9d7e177238a96f3dbd2b7a7e55dc112f0d0
Tuyet Linh <tuyetlnhh@gmail.com>
2023-07-21 14:45:21 +0700

    sample app: box color

--- src/pages/BoxColor.jsx
+++ src/pages/BoxColor.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Button from '../components/Button'
+import styles from './boxColor.module.css' 
 
 function BoxColor() {
+  const [color, setColor] = React.useState(null);
+
+  function handleChangeColor(newColor) {
+    newColor == color ? setColor(null) : setColor(newColor);
+  }
   return (
-    <div>BoxColor</div>
+    <div className={styles.container}>
+      <div className={styles.box}
+        style = {{
+          backgroundColor: !color ? 'red' : color
+        }}
+        onClick={() => handleChangeColor('red')}>
+          Red
+      </div>
+
+      <div className={styles.box}
+        style = {{
+          backgroundColor: !color ? 'gold' : color
+        }}
+        onClick={() => handleChangeColor('gold')}>
+          Gold
+      </div>
+
+      <div className={styles.box}
+        style = {{
+          backgroundColor: !color ? 'aqua' : color
+        }}
+        onClick={() => handleChangeColor('aqua')}>
+          Aqua
+      </div>
+
+      <div className={styles.box}
+        style = {{
+          backgroundColor: !color ? 'purple' : color
+        }}
+        onClick={() => handleChangeColor('purple')}>
+          Purple
+      </div>
+    </div>
   )
 }
 
